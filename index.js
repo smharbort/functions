9a35c338bb53a265edae51fcfa5ca132cde3f97d
smh <smharbort@outlook.com>
2024-10-02 11:18:57 -0400

    initializing and some function notes

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+                                                        JS FUNCTION CORRECT SYNTAXES
+
+function functionName (parameters) {}
+
+const variableName = function (parameters) {}
+
+const variableName = (parameters) => expression         // is the same as 
+                                                        // const add = (a, b) => a + b
+                                                        // without
+
+note: as far as I understand, code in-function cannot execute after return statements
+
+*/
+
